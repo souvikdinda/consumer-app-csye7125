d5d3a4896fbd943d9dec13b01096804fe235214a
NagaVaishnavi-Puppala <puppala.n@northeastern.edu>
2023-11-10 11:42:35 -0500

    fix: Updated Postgres setup

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+
 import consumer from './consumers/kafkaconsumer.js';
-import message from './models/messageModel';
+import uptime from './model/messageModel.js';
+import sequelize from './model/index.js'
+
+// Bootstrapping DatabaseError
+sequelize.sync({alter: false, force: false}).then((data) => {
+  console.log("Database connection established")
+}).catch((err) => {
+  console.error("Couldn't connect to database\n", err)
+})
 
 const run = async () => {
   try {
@@ -9,21 +18,19 @@ const run = async () => {
     await consumer.run({
       eachMessage: async ({ topic, partition, message }) => {
         const messageContent = message.value.toString();
-        const messageData = JSON.parse(messageContent);
-        const { uri, status } = messageData; 
-
+        console.log('Received message from Kafka producer:', messageContent);
         try {
-         
-          const existingMessage = await message.findOne({ where: { uri } })
-          if (existingMessage) {
-            await existingMessage.update({ status });
-            console.log('Status updated for existing URI:', uri, 'New status:', status);
-          } else {
-            await message.create({ uri, status, content: messageContent });
-            console.log('New message created with URI:', uri, 'and status:', status);
+          const messageData = JSON.parse(messageContent)
+          const { uri, status } = messageData
+          console.log("Message received for uri:", uri, " and status is:", status)
+
+          try {
+              await uptime.create({ uri, status, content: messageContent });
+          } catch (error) {
+            console.error('Error processing message:', error);
           }
-        } catch (error) {
-          console.error('Error processing message:', error);
+        } catch(error) {
+          console.log(error)
         }
       },
     });
@@ -33,3 +40,4 @@ const run = async () => {
 };
 
 run().catch(console.error);
+
